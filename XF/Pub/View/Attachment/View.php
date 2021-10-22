ed40ef82b052751b9c21e4beb824e35e4af4ef40
KirbyDE <akirbach@forumhome.com>
2021-10-22 07:30:34 +0200

    Simplified code
    Removed listener
    Removed globals
    Replaced array_key_exists() with isset()

--- XF/Pub/View/Attachment/View.php
+++ XF/Pub/View/Attachment/View.php
@@ -3,21 +3,8 @@
 namespace TickTackk\ImageAttachmentCacheControl\XF\Pub\View\Attachment;
 
 use TickTackk\ImageAttachmentCacheControl\Pub\View\AttachmentTrait as AttachmentViewTrait;
-use XF\Http\ResponseStream as HttpResponseStream;
 
 class View extends XFCP_View
 {
-    use AttachmentViewTrait;
-
-    /**
-     * @return string|HttpResponseStream
-     *
-     * @throws \Exception
-     */
-    public function renderRaw()
-    {
-        $this->setupForTckImageAttachmentCacheControl();
-
-        return parent::renderRaw();
-    }
+	use AttachmentViewTrait;
 }
